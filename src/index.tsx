62059c0e18cd209b1a42df13a2624370450a8495
Jorge Shirai <jlshirai@gmail.com>
2019-01-09 11:23:58 -0300

    Add src/components folder.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Web3Provider from 'react-web3-provider'
-import App from './App'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
 
